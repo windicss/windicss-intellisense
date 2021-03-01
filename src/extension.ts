ec6390b8fd405cbad6fb9feb3015b4fa2e7f470b
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-03-01 18:45:28 +0100

    feat: sort on save
    add option to sort classes on save

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import { registerCompletions } from './lib/completions';
 import { registerCommands } from './lib/commands';
 import { registerCodeFolding } from './lib/folding';
 import { init } from './lib/core';
-import { workspace } from 'vscode';
+import { commands, workspace } from 'vscode';
 import type { Core } from './interfaces';
 import type { ExtensionContext } from 'vscode';
 
@@ -32,6 +32,16 @@ export async function activate(ctx: ExtensionContext) {
   registerCompletions(ctx, CORE);
   registerCodeFolding(ctx);
   registerCommands(ctx, CORE);
+
+  // if runOnSave is enabled in settings, trigger command on file save
+  if(workspace.getConfiguration().get('windicss.runOnSave')) {
+    ctx.subscriptions.push(
+      workspace.onDidSaveTextDocument((_e) => {
+        commands.executeCommand('windicss.sort');
+      })
+    );
+  }
+
   console.log('Windi CSS Intellisense is now active!');
 }
 
