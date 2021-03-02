6044847a5ecb7c462ea9fbab3051f3350b01b1bd
voorjaar <felitas@zoho.com>
2021-03-02 18:43:39 +0800

    build: update workflow

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+const { ModuleResolutionKind } = require('typescript');
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'abc': '#1c1c1e',
+        'def': '#fff',
+      },
+    },
+  },
+};
