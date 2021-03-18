c636fa4a9d306b37650310fc6c2bd76ddacfa3f8
voorjaar <felitas@zoho.com>
2021-03-18 08:04:22 +0800

    build: update dependency version

--- src/utils/filetypes.ts
+++ src/utils/filetypes.ts
@@ -52,44 +52,44 @@ export const fileTypes: {
     splitCharacter: string;
   }[];
 }[] = [
-    {
-      extension: 'css',
-      patterns: stylesPatterns,
-    },
-    {
-      extension: 'sass',
-      patterns: stylesPatterns,
-    },
-    {
-      extension: 'less',
-      patterns: stylesPatterns,
-    },
-    {
-      extension: 'javascript',
-      patterns: jsPatterns,
-    },
-    {
-      extension: 'javascriptreact',
-      patterns: jsPatterns,
-    },
-    {
-      extension: 'typescriptreact',
-      patterns: jsPatterns,
-    },
-    {
-      extension: 'html',
-      patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
-    },
-    {
-      extension: 'php',
-      patterns: htmlPatterns,
-    },
-    {
-      extension: 'vue',
-      patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
-    },
-    {
-      extension: 'svelte',
-      patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
-    },
-  ];
+  {
+    extension: 'css',
+    patterns: stylesPatterns,
+  },
+  {
+    extension: 'sass',
+    patterns: stylesPatterns,
+  },
+  {
+    extension: 'less',
+    patterns: stylesPatterns,
+  },
+  {
+    extension: 'javascript',
+    patterns: jsPatterns,
+  },
+  {
+    extension: 'javascriptreact',
+    patterns: jsPatterns,
+  },
+  {
+    extension: 'typescriptreact',
+    patterns: jsPatterns,
+  },
+  {
+    extension: 'html',
+    patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
+  },
+  {
+    extension: 'php',
+    patterns: htmlPatterns,
+  },
+  {
+    extension: 'vue',
+    patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
+  },
+  {
+    extension: 'svelte',
+    patterns: htmlPatterns.concat(stylesPatterns, attributePatterns),
+  },
+];
