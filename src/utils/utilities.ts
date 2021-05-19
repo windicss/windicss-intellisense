4fde5cc449849edb31cfbb19c263c14ce868aa68
voorjaar <felitas@zoho.com>
2021-05-19 14:37:10 +0800

    feat: support v3.0 utilities completion

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,254 +1,39 @@
 const utilities: { [key:string]: string[]} = {
-  // Layout
-  container: [
-    'container',
+  animation: [
+    'animate-${static}',
   ],
-  objectPosition: [
-    'object-${static}',
+  backdropBlur: [
+    'backdrop-blur-${static}',
   ],
-  inset: [
-    'inset-${static}',
-    'inset-${float}',
-    'inset-${fraction}',
-    'inset-${size}',
-
-    'inset-y-${static}',
-    'inset-y-${float}',
-    'inset-y-${fraction}',
-    'inset-y-${size}',
-
-    'inset-x-${static}',
-    'inset-x-${float}',
-    'inset-x-${fraction}',
-    'inset-x-${size}',
-
-    'top-${static}',
-    'top-${float}',
-    'top-${fraction}',
-    'top-${size}',
-
-    'right-${static}',
-    'right-${float}',
-    'right-${fraction}',
-    'right-${size}',
-
-    'bottom-${static}',
-    'bottom-${float}',
-    'bottom-${fraction}',
-    'bottom-${size}',
-
-    'left-${static}',
-    'left-${float}',
-    'left-${fraction}',
-    'left-${size}',
+  backdropBrightness: [
+    'backdrop-brightness-${static}',
   ],
-  zIndex: [
-    'z-${static}',
-    'z-${int}',
+  backdropContrast: [
+    'backdrop-contrast-${static}',
   ],
-  // Flexbox
-  flex: [
-    'flex-${static}',
+  backdropGrayscale: [
+    'backdrop-grayscale-${static}',
   ],
-  flexGrow: [
-    'flex-grow-${static}',
+  backdropHueRotate: [
+    'backdrop-hue-rotate-${static}',
   ],
-  flexShrink: [
-    'flex-shrink-${static}',
+  backdropInvert: [
+    'backdrop-invert-${static}',
   ],
-  order: [
-    'order-${static}',
-    'order-${int}',
+  backdropOpacity: [
+    'backdrop-opacity-${static}',
   ],
-  // Grid
-  gridTemplateColumns: [
-    'grid-cols-${static}',
-    'grid-cols-${int}',
+  backdropSaturate: [
+    'backdrop-saturate-${static}',
   ],
-  gridTemplateRows: [
-    'grid-rows-${static}',
-    'grid-rows-${int}',
+  backdropSepia: [
+    'backdrop-sepia-${static}',
   ],
-  gridColumn: [
-    'col-${static}',
-    'col-span-${int}',
-  ],
-  gridColumnEnd: [
-    'col-end-${static}',
-    'col-end-${int}',
-  ],
-  gridColumnStart: [
-    'col-start-${static}',
-    'col-start-${int}',
-  ],
-  gridRow: [
-    'row-${static}',
-    'row-span-${int}',
-  ],
-  gridRowEnd: [
-    'row-end-${static}',
-    'row-end-${int}',
-  ],
-  gridRowStart: [
-    'row-start-${static}',
-    'row-start-${int}',
-  ],
-  gap: [
-    'gap-${static}',
-    'gap-x-${static}',
-    'gap-y-${static}',
-
-    'gap-${float}',
-    'gap-x-${float}',
-    'gap-y-${float}',
-
-    'gap-${size}',
-    'gap-x-${size}',
-    'gap-y-${size}',
-  ],
-  // Box Alignment
-  // Spacing
-  padding: [
-    'p-${static}',
-    'py-${static}',
-    'px-${static}',
-    'pt-${static}',
-    'pr-${static}',
-    'pb-${static}',
-    'pl-${static}',
-
-    'p-${float}',
-    'py-${float}',
-    'px-${float}',
-    'pt-${float}',
-    'pr-${float}',
-    'pb-${float}',
-    'pl-${float}',
-
-    'p-${size}',
-    'py-${size}',
-    'px-${size}',
-    'pt-${size}',
-    'pr-${size}',
-    'pb-${size}',
-    'pl-${size}',
-  ],
-  margin: [
-    'm-${static}',
-    'my-${static}',
-    'mx-${static}',
-    'mt-${static}',
-    'mr-${static}',
-    'mb-${static}',
-    'ml-${static}',
-
-    'm-${float}',
-    'my-${float}',
-    'mx-${float}',
-    'mt-${float}',
-    'mr-${float}',
-    'mb-${float}',
-    'ml-${float}',
-
-    'm-${size}',
-    'my-${size}',
-    'mx-${size}',
-    'mt-${size}',
-    'mr-${size}',
-    'mb-${size}',
-    'ml-${size}',
-  ],
-  space: [
-    'space-y-${static}',
-    'space-y-reverse',
-    'space-x-${static}',
-    'space-x-reverse',
-    'space-y-${float}',
-    'space-x-${float}',
-  ],
-  width: [
-    'w-${static}',
-    'w-${float}',
-    'w-${fraction}',
-    'w-${int}xl',
-    'w-${size}',
-  ],
-  minWidth: [
-    'min-w-${static}',
-    'min-w-${float}',
-    'min-w-${fraction}',
-    'min-w-${int}xl',
-    'min-w-${size}',
-  ],
-  maxWidth: [
-    'max-w-${static}',
-    'max-w-${float}',
-    'max-w-${fraction}',
-    'max-w-${int}xl',
-    'max-w-${size}',
-  ],
-  height: [
-    'h-${static}',
-    'h-${float}',
-    'h-${fraction}',
-    'h-${int}xl',
-    'h-${size}',
-  ],
-  minHeight: [
-    'min-h-${static}',
-    'min-h-${float}',
-    'min-h-${fraction}',
-    'min-h-${int}xl',
-    'min-h-${size}',
-  ],
-  maxHeight: [
-    'max-h-${static}',
-    'max-h-${float}',
-    'max-h-${fraction}',
-    'max-h-${int}xl',
-    'max-h-${size}',
-  ],
-  // Typography
-  fontSize: [
-    'text-${static}',
-    'text-${int}xl',
-  ],
-  textOpacity: [
-    'text-opacity-${static}',
-    'text-opacity-${int<=100}',
-  ],
-  textColor: [
-    'text-${color}',
-  ],
-  fontFamily: [
-    'font-${static}',
-  ],
-  fontWeight: [
-    'font-${static}',
-    'font-${int}',
-  ],
-  letterSpacing: [
-    'tracking-${static}',
-    'tracking-${size}',
-  ],
-  lineHeight: [
-    'leading-${static}',
-    'leading-${int}',
-    'leading-${size}',
-  ],
-  listStyleType: [
-    'list-${static}',
-  ],
-  placeholderColor: [
-    'placeholder-${color}',
-  ],
-  placeholderOpacity: [
-    'placeholder-opacity-${static}',
-    'placeholder-opacity-${int<=100}',
-  ],
-  // Backgrounds
   backgroundColor: [
     'bg-${color}',
+  ],
+  backgroundImage: [
+    'bg-${static}',
   ],
   backgroundOpacity: [
     'bg-opacity-${static}',
@@ -260,15 +45,18 @@ const utilities: { [key:string]: string[]} = {
   backgroundSize: [
     'bg-${static}',
   ],
-  backgroundImage: [
-    'bg-${static}',
+  blur: [
+    'blur-${static}',
+    'blur-${int}',
+    'blur-${size}',
   ],
-  gradientColorStops: [
-    'from-${color}',
-    'via-${color}',
-    'to-${color}',
+  borderColor: [
+    'border-${color}',
   ],
-  // Borders
+  borderOpacity: [
+    'border-opacity-${static}',
+    'border-opacity-${int<=100}',
+  ],
   borderRadius: [
     'rounded-${static}',
     'rounded-t-${static}',
@@ -312,18 +100,29 @@ const utilities: { [key:string]: string[]} = {
     'border-l-${int}',
     'border-l-${size}',
   ],
-  borderColor: [
-    'border-${color}',
+  boxShadow: [
+    'shadow-${static}',
   ],
-  borderOpacity: [
-    'border-opacity-${static}',
-    'border-opacity-${int<=100}',
+  boxShadowColor: [
+    'shadow-${color}',
   ],
-  divideWidth: [
-    'divide-y-reverse',
-    'divide-x-reverse',
-    'divide-y-${int}',
-    'divide-x-${int}',
+  brightness: [
+    'brightness-${static}',
+  ],
+  caretColor: [
+    'caret-${color}',
+  ],
+  caretOpacity: [
+    'caret-opacity-${static}',
+  ],
+  container: [
+    'container',
+  ],
+  contrast: [
+    'contrast-${static}',
+  ],
+  cursor: [
+    'cursor-${static}',
   ],
   divideColor: [
     'divide-${color}',
@@ -332,86 +131,222 @@ const utilities: { [key:string]: string[]} = {
     'divide-${static}',
     'divide-opacity-${int<=100}',
   ],
-  ringOffsetWidth: [
-    'ring-offset-${static}',
-    'ring-offset-${int}',
+  divideWidth: [
+    'divide-y-reverse',
+    'divide-x-reverse',
+    'divide-y-${int}',
+    'divide-x-${int}',
   ],
-  ringOffsetColor: [
-    'ring-offset-${color}',
+  dropShadow: [
+    'drop-shadow-${static}',
   ],
-  ringWidth: [
-    'ring-${static}',
-    'ring-${int}',
+  fill: [
+    'fill-${color}',
   ],
-  ringColor: [
-    'ring-${color}',
+  flex: [
+    'flex-${static}',
   ],
-  ringOpacity: [
-    'ring-${static}',
-    'ring-opacity-${int<=100}',
+  flexGrow: [
+    'flex-grow-${static}',
   ],
-  // Effects
-  boxShadow: [
-    'shadow-${static}',
+  flexShrink: [
+    'flex-shrink-${static}',
+  ],
+  fontFamily: [
+    'font-${static}',
+  ],
+  fontSize: [
+    'text-${static}',
+    'text-${int}xl',
+  ],
+  fontWeight: [
+    'font-${static}',
+    'font-${int}',
+  ],
+  gap: [
+    'gap-${static}',
+    'gap-x-${static}',
+    'gap-y-${static}',
+
+    'gap-${float}',
+    'gap-x-${float}',
+    'gap-y-${float}',
+
+    'gap-${size}',
+    'gap-x-${size}',
+    'gap-y-${size}',
+  ],
+  gradientColorStops: [
+    'from-${color}',
+    'via-${color}',
+    'to-${color}',
+  ],
+  grayscale: [
+    'grayscale-${static}',
+  ],
+  gridAutoColumns: [
+    'auto-cols-${static}',
+  ],
+  gridAutoRows: [
+    'auto-rows-${static}',
+  ],
+  gridColumn: [
+    'col-${static}',
+    'col-span-${int}',
+  ],
+  gridColumnEnd: [
+    'col-end-${static}',
+    'col-end-${int}',
+  ],
+  gridColumnStart: [
+    'col-start-${static}',
+    'col-start-${int}',
+  ],
+  gridRow: [
+    'row-${static}',
+    'row-span-${int}',
+  ],
+  gridRowEnd: [
+    'row-end-${static}',
+    'row-end-${int}',
+  ],
+  gridRowStart: [
+    'row-start-${static}',
+    'row-start-${int}',
+  ],
+  gridTemplateColumns: [
+    'grid-cols-${static}',
+    'grid-cols-${int}',
+  ],
+  gridTemplateRows: [
+    'grid-rows-${static}',
+    'grid-rows-${int}',
+  ],
+  height: [
+    'h-${static}',
+    'h-${float}',
+    'h-${fraction}',
+    'h-${int}xl',
+    'h-${size}',
+  ],
+  hueRotate: [
+    'hue-rotate-${static}',
+  ],
+  inset: [
+    'inset-${static}',
+    'inset-${float}',
+    'inset-${fraction}',
+    'inset-${size}',
+
+    'inset-y-${static}',
+    'inset-y-${float}',
+    'inset-y-${fraction}',
+    'inset-y-${size}',
+
+    'inset-x-${static}',
+    'inset-x-${float}',
+    'inset-x-${fraction}',
+    'inset-x-${size}',
+
+    'top-${static}',
+    'top-${float}',
+    'top-${fraction}',
+    'top-${size}',
+
+    'right-${static}',
+    'right-${float}',
+    'right-${fraction}',
+    'right-${size}',
+
+    'bottom-${static}',
+    'bottom-${float}',
+    'bottom-${fraction}',
+    'bottom-${size}',
+
+    'left-${static}',
+    'left-${float}',
+    'left-${fraction}',
+    'left-${size}',
+  ],
+  invert: [
+    'invert-${static}',
+  ],
+  letterSpacing: [
+    'tracking-${static}',
+    'tracking-${size}',
+  ],
+  lineHeight: [
+    'leading-${static}',
+    'leading-${int}',
+    'leading-${size}',
+  ],
+  listStyleType: [
+    'list-${static}',
+  ],
+  margin: [
+    'm-${static}',
+    'my-${static}',
+    'mx-${static}',
+    'mt-${static}',
+    'mr-${static}',
+    'mb-${static}',
+    'ml-${static}',
+
+    'm-${float}',
+    'my-${float}',
+    'mx-${float}',
+    'mt-${float}',
+    'mr-${float}',
+    'mb-${float}',
+    'ml-${float}',
+
+    'm-${size}',
+    'my-${size}',
+    'mx-${size}',
+    'mt-${size}',
+    'mr-${size}',
+    'mb-${size}',
+    'ml-${size}',
+  ],
+  maxHeight: [
+    'max-h-${static}',
+    'max-h-${float}',
+    'max-h-${fraction}',
+    'max-h-${int}xl',
+    'max-h-${size}',
+  ],
+  maxWidth: [
+    'max-w-${static}',
+    'max-w-${float}',
+    'max-w-${fraction}',
+    'max-w-${int}xl',
+    'max-w-${size}',
+  ],
+  minHeight: [
+    'min-h-${static}',
+    'min-h-${float}',
+    'min-h-${fraction}',
+    'min-h-${int}xl',
+    'min-h-${size}',
+  ],
+  minWidth: [
+    'min-w-${static}',
+    'min-w-${float}',
+    'min-w-${fraction}',
+    'min-w-${int}xl',
+    'min-w-${size}',
+  ],
+  objectPosition: [
+    'object-${static}',
   ],
   opacity: [
     'opacity-${static}',
     'opacity-${int<=100}',
   ],
-  transition: [
-    'transition-${static}',
+  order: [
+    'order-${static}',
+    'order-${int}',
   ],
-  transitionDuration: [
-    'duration-${static}',
-    'duration-${int}',
-  ],
-  transitionTimingFunction: [
-    'ease-${static}',
-  ],
-  transitionDelay: [
-    'delay-${static}',
-    'delay-${int}',
-  ],
-  animation: [
-    'animate-${static}',
-  ],
-  // Transforms
-  transformOrigin: [
-    'origin-${static}',
-  ],
-  scale: [
-    'scale-${static}',
-    'scale-${int}',
-    'scale-x-${static}',
-    'scale-x-${int}',
-    'scale-y-${static}',
-    'scale-y-${int}',
-  ],
-  rotate: [
-    'rotate-${static}',
-    'rotate-${float}',
-  ],
-  translate: [
-    'translate-${static}',
-    'translate-x-${static}',
-    'translate-y-${static}',
-    'translate-x-${float}',
-    'translate-x-${fraction}',
-    'translate-x-${size}',
-    'translate-y-${float}',
-    'translate-y-${fraction}',
-    'translate-y-${size}',
-  ],
-  skew: [
-    'skew-x-${static}',
-    'skew-x-${float}',
-    'skew-y-${static}',
-    'skew-y-${float}',
-  ],
-  cursor: [
-    'cursor-${static}',
-  ],
-  // Interactivity
   outline: [
     'outline-${static}',
   ],
@@ -420,16 +355,200 @@ const utilities: { [key:string]: string[]} = {
     'outline-solid-${color}',
     'outline-dotted-${color}',
   ],
-  // SVG
-  fill: [
-    'fill-${color}',
+  padding: [
+    'p-${static}',
+    'py-${static}',
+    'px-${static}',
+    'pt-${static}',
+    'pr-${static}',
+    'pb-${static}',
+    'pl-${static}',
+
+    'p-${float}',
+    'py-${float}',
+    'px-${float}',
+    'pt-${float}',
+    'pr-${float}',
+    'pb-${float}',
+    'pl-${float}',
+
+    'p-${size}',
+    'py-${size}',
+    'px-${size}',
+    'pt-${size}',
+    'pr-${size}',
+    'pb-${size}',
+    'pl-${size}',
   ],
-  // Stroke
+  perspective: [
+    'perspect-${static}',
+  ],
+  perspectiveOrigin: [
+    'perspect-origin-${static}',
+  ],
+  placeholderColor: [
+    'placeholder-${color}',
+  ],
+  placeholderOpacity: [
+    'placeholder-opacity-${static}',
+    'placeholder-opacity-${int<=100}',
+  ],
+  ringColor: [
+    'ring-${color}',
+  ],
+  ringOffsetColor: [
+    'ring-offset-${color}',
+  ],
+  ringOffsetWidth: [
+    'ring-offset-${static}',
+    'ring-offset-${int}',
+  ],
+  ringOpacity: [
+    'ring-${static}',
+    'ring-opacity-${int<=100}',
+  ],
+  ringWidth: [
+    'ring-${static}',
+    'ring-${int}',
+  ],
+  rotate: [
+    'rotate-${static}',
+    'rotate-x-${static}',
+    'rotate-y-${static}',
+    'rotate-z-${static}',
+    'rotate-${float}',
+    'rotate-x-${float}',
+    'rotate-y-${float}',
+    'rotate-z-${float}',
+  ],
+  saturate: [
+    'saturate-${static}',
+  ],
+  scale: [
+    'scale-${static}',
+    'scale-${int}',
+    'scale-x-${static}',
+    'scale-x-${int}',
+    'scale-y-${static}',
+    'scale-y-${int}',
+    'scale-z-${static}',
+    'scale-z-${int}',
+  ],
+  sepia: [
+    'sepia-${static}',
+  ],
+  skew: [
+    'skew-x-${static}',
+    'skew-x-${float}',
+    'skew-y-${static}',
+    'skew-y-${float}',
+  ],
+  space: [
+    'space-y-${static}',
+    'space-y-reverse',
+    'space-x-${static}',
+    'space-x-reverse',
+    'space-y-${float}',
+    'space-x-${float}',
+  ],
   stroke: [
     'stroke-${color}',
   ],
   strokeWidth: [
+    'stroke-${static}',
     'stroke-${int}',
+  ],
+  strokeDashArray: [
+    'stroke-dash-${static}',
+    'stroke-dash-${int}',
+  ],
+  strokeDashOffset: [
+    'stroke-offset-${static}',
+    'stroke-offset-${int}',
+  ],
+  tabSize: [
+    'tab-${static}',
+  ],
+  textColor: [
+    'text-${color}',
+  ],
+  textOpacity: [
+    'text-opacity-${static}',
+    'text-opacity-${int<=100}',
+  ],
+  textShadow: [
+    'text-shadow-${static}',
+  ],
+  textDecorationColor: [
+    'underline-${color}',
+  ],
+  textDecorationOpacity: [
+    'underline-opacity-${static}',
+    'underline-opacity-${int<=100}',
+  ],
+  textDecorationLength: [
+    'underline-${static}',
+    'underline-${int}',
+    'underline-${size}',
+  ],
+  textDecorationOffset: [
+    'underline-offset-${static}',
+    'underline-offset-${int}',
+    'underline-offset-${size}',
+  ],
+  textIndent: [
+    'indent-${static}',
+    'indent-${size}',
+  ],
+  textStrokeColor: [
+    'text-stroke-${color}',
+  ],
+  textStrokeWidth: [
+    'text-stroke-${static}',
+    'text-stroke-${size}',
+  ],
+  transformOrigin: [
+    'origin-${static}',
+  ],
+  transitionDuration: [
+    'duration-${static}',
+    'duration-${int}',
+  ],
+  transitionDelay: [
+    'delay-${static}',
+    'delay-${int}',
+  ],
+  transitionProperty: [
+    'transition-${static}',
+  ],
+  transitionTimingFunction: [
+    'ease-${static}',
+  ],
+  translate: [
+    'translate-${static}',
+    'translate-x-${static}',
+    'translate-y-${static}',
+    'translate-z-${static}',
+    'translate-x-${float}',
+    'translate-x-${fraction}',
+    'translate-x-${size}',
+    'translate-y-${float}',
+    'translate-y-${fraction}',
+    'translate-y-${size}',
+    'translate-z-${float}',
+    'translate-z-${fraction}',
+    'translate-z-${size}',
+  ],
+  width: [
+    'w-${static}',
+    'w-${float}',
+    'w-${fraction}',
+    'w-${int}xl',
+    'w-${size}',
+  ],
+  zIndex: [
+    'z-${static}',
+    'z-${int}',
   ],
 
   // Plugins
@@ -456,17 +575,6 @@ const utilities: { [key:string]: string[]} = {
   lineClamp: [
     'line-clamp-none',
     'line-clamp-${int}',
-  ],
-  filter: [
-    'filter-${static}',
-  ],
-  backdropFilter: [
-    'backdrop-${static}',
-  ],
-  blur: [
-    'blur-${static}',
-    'blur-${float}',
-    'blur-${size}',
   ],
 };
 
