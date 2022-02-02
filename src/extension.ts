75ac27c98c7cf782a24829d2b7b0610ff75e6bf7
Alexander Niebuhr <tech@nbhr.io>
2022-02-02 10:42:58 +0100

    fix: :bug: wrong glob pattern for activationEvents

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
-import Extension from './lib';
-import { Log } from './utils/console';
+import Extension from "./lib";
+import { Log } from "./utils/console";
 import {
   ExtensionContext,
   RelativePattern,
   workspace as Workspace,
-} from 'vscode';
+} from "vscode";
 
-const CONFIG_FILE_GLOB = '{tailwind,windi}.config.{js,cjs,mjs,ts}';
+const CONFIG_FILE_GLOB = "{tailwind,windi}.config.{js,cjs,mjs,ts}";
 
 export async function activate(ctx: ExtensionContext) {
   const extension = new Extension(
@@ -19,7 +19,7 @@ export async function activate(ctx: ExtensionContext) {
   extension.init();
   extension.watch();
   extension.registerCodeFolding();
-  Log.info('Windi CSS Intellisense Is Now Active!');
+  Log.info("Windi CSS Intellisense Is Now Active!");
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
